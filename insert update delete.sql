62136f4235c55858a81b36b6af049e2f02dbf16e
rajatkumar0308 <rajat.kumar03081993@gmail.com>
2025-02-19 10:44:32 +0530

    Add files via upload

---
+++ insert update delete.sql
@@ -0,0 +1,28 @@
+# SELECT, INSERT, UPDATE, DELETE
+
+#CREATE TABLE world.EMPLOYE (
+#id INT AUTO_INCREMENT PRIMARY KEY,
+#name VARCHAR(100),
+#position VARCHAR(100),
+#salary DECIMAL(10,2)
+#);
+
+#INSERT INTO world.EMPLOYE (name, position, salary)
+#value
+#("rajat","Senior AVP",30000.00),
+#("suresh","Analyst",5000.20),
+#("Tom","HR",15000.20),
+#("Wayne","Manager",55000.20);
+
+#SELECT * FROM world.EMPLOYE;
+
+#UPDATE world.EMPLOYE
+#SET salary = 70000.00
+#WHERE id = "3";
+
+#SELECT * FROM world.EMPLOYE;
+
+#DELETE FROM world.EMPLOYE
+#WHERE id = "3";
+
+SELECT * FROM world.EMPLOYE;
